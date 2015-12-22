08f1165e1422232e4164aa0768e1337ff43ab2ff
ldonis <ldonis.emc2@gmail.com>
2015-12-21 20:22:19 -0600

    Remove border for hr tags

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-git');
 
     ///* ~·~ ~·~ ~·~ ~·~ ~·~ ~·~
     ///* Running/defining tasks
