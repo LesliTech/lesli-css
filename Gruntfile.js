0e74332369c39891bd7a07b4d2392d4e151698a7
ldonis <ldonis.emc2@gmail.com>
2015-12-25 21:34:57 -0600

    add build task to copy files

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
+    ///* ~·~ ~·~ ~·~ ~·~ ~·~ ~·~ ~·~ ~·~
+    ///* Running/defining all tasks
+    ///* ~·~ ~·~ ~·~ ~·~ ~·~ ~·~ ~·~ ~·~
+    grunt.registerTask('build', ['copy']);
+
     ///* ~·~ ~·~ ~·~ ~·~ ~·~ ~·~
     ///* Running/defining tasks
     ///* ~·~ ~·~ ~·~ ~·~ ~·~ ~·~
