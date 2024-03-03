f0aad8f580c6a6784e27a88b13b12830dbfae267
Luis Donis <ldonis.emc2@gmail.com>
2024-03-02 19:56:51 -0600

    feat: structure refactor

--- tests/scss.spec.js
+++ tests/scss.spec.js
@@ -1,12 +1,51 @@
-const path = require('path')
-const sassTrue = require('sass-true')
-const glob = require('glob')
- 
-describe('Running SASS tests', () => {
+/*
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+LesliCSS - SCSS Utilities for websites, apps and web applications
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/
+
+
+// · 
+import { describe, it } from "vitest";
+
+
+// · 
+const path = require("path")
+const glob = require("glob")
+const sassTrue = require("sass-true")
+
+
+// · 
+describe("RUNNING SASS TESTS", () => {
 
     // Find all of the Sass files that end in `*.spec.scss` in any directory of this project.
     // I use path.resolve because True requires absolute paths to compile test files.
-    const sassTestFiles = glob.sync(path.resolve(process.cwd(), 'tests/**/*.spec.scss'))
+    const sassTestFiles = glob.sync(path.resolve(process.cwd(), "tests/**/*.spec.scss"))
 
     // Run True on every file found with the describe and it methods provided
     sassTestFiles.forEach(file => sassTrue.runSass({ file }, { describe, it }))
